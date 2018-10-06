76f6716dec61a2189a72a42fcb3663c4dd488771
mkunikow <michal.kunikowski@gmail.com>
2018-10-06 22:13:25 +0200

    init

---
+++ src/main/java/Main.java
@@ -0,0 +1,75 @@
+import java.time.*;
+
+/**
+ * ZonedDateTime is fully aware of DST.
+ *
+ * For an example, let's take a country where DST is fully observed, like Italy (UTC/GMT +2).
+ *
+ * In 2015, DST started in Italy on March, 29th and ended on October, 25th. This means that on:
+ *
+ * March, 29 2015 at 2:00:00 A.M. clocks were turned forward 1 hour to
+ * March, 29 2015 at 3:00:00 A.M. local daylight time instead
+ * (So a time like March, 29 2015 2:30:00 A.M. didn't actually exist!)
+ *
+ * October, 25 2015 at 3:00:00 A.M. clocks were turned backward 1 hour to
+ * October, 25 2015 at 2:00:00 A.M. local daylight time instead
+ * (So a time like October, 25 2015 2:30:00 A.M. actually existed twice!)
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        LocalDateTime ldt = LocalDateTime.of(2015, 3, 29, 2, 30);
+        System.out.println(ldt);
+
+        // But if we create an instance of ZonedDateTime for Italy (notice that the format uses a city, not a country) and printed:
+        ZonedDateTime zdt = ZonedDateTime.of(
+                2015, 3, 29, 1, 30, 0, 0, ZoneId.of("Europe/Rome"));
+        System.out.println(zdt);
+
+        zdt = ZonedDateTime.of(
+                2015, 3, 29, 2, 30, 0, 0, ZoneId.of("Europe/Rome"));
+        System.out.println(zdt);
+
+
+        zdt = ZonedDateTime.of(
+                2015, 3, 29, 3, 30, 0, 0, ZoneId.of("Europe/Rome"));
+        System.out.println(zdt);
+
+
+        // But be careful. We have to use a regional ZoneId,
+        // a ZoneOffset won't do the trick because this class doesn't have the zone rules information to account for DST:
+
+        ZonedDateTime zdt1 = ZonedDateTime.of(
+                2015, 3, 29, 2, 30, 0, 0, ZoneOffset.ofHours(2));
+        ZonedDateTime zdt2 = ZonedDateTime.of(
+                2015, 3, 29, 2, 30, 0, 0, ZoneId.of("UTC+2"));
+        System.out.println(zdt1); System.out.println(zdt2);
+
+        //        When DST ends, something similar happens
+
+        ldt = LocalDateTime.of(2015, 10, 25, 3, 30);
+        System.out.println(ldt);
+
+        // When we create an instance of ZonedDateTime for Italy, we have to add an hour to see the effect
+        // (otherwise we we'll be creating the ZonedDateTime at 3:00 of the new time)
+
+        zdt = ZonedDateTime.of(
+                2015, 10, 25, 2, 30, 0, 0, ZoneId.of("Europe/Rome"));
+        zdt2 = zdt.plusHours(1);
+        System.out.println(zdt);
+        System.out.println(zdt2);
+
+
+        // We also need to be careful when adjusting the time across the DST boundary with a version of the methods plus()
+        //   and minus() that takes a TemporalAmount implementation, in other words, a Period or a Duration.
+        // This is because both differ in their treatment of daylight savings time.
+
+        // Consider one hour before the beginning of DST in Italy:
+
+        zdt = ZonedDateTime.of(
+                2015, 3, 29, 1, 0, 0, 0, ZoneId.of("Europe/Rome"));
+
+        System.out.println(zdt.plus(Duration.ofDays(1)));
+        System.out.println(zdt.plus(Period.ofDays(1)));
+    }
+}
